906d809624895cbfee6fe2517aae4190d5ae61de
Thanigaivel <142864072+Thanigaivelelango@users.noreply.github.com>
2023-10-25 21:34:33 +0530

    Update settings.py

--- conversiontool/settings.py
+++ conversiontool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)f*)(&9qb3mvrxhk=sqtim7j44f$dkh+*l8%ch=gjcz0-0ll^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
